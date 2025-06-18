465082d79e8f1d3ac569f3c2b1675669b7633750
Nguyenthituyetnhat <nguyenthituyetnhat2k3@gmail.com>
2025-06-18 12:08:21 +0700

    commit

--- resources/views/site/pages/main.blade.php
+++ resources/views/site/pages/main.blade.php
@@ -9,171 +9,38 @@
             {{-- {{
                 print_r($class)
             }} --}}
-            <?php
-            foreach ($class as $row):
-                echo $row->avatar;
-            endforeach;
-            ?>
+          
             <!--panel-Body-->
             <div class="panel-body">
                 <div class="content-row px-3">
                     <!-- card-start -->
                     <div class="row">
-                        <div class="col-12 col-md-6 col-lg-4 p-3">
-                            <div class="card">
-                                <div class="bg-card card-img-top"
-                                    style="--bg-avatar: url('{{ URL::to(config('asset.images_path') . 'bgrk.png') }}')">
-                                </div>
+                          <?php
+                        if(isset($class) && !empty($class)):
+                        foreach ($class as $row):?>
+                            <div class="col-12 col-md-6 col-lg-4 p-3">
+                                <div class="card">
+                                    <div class="bg-card card-img-top"
+                                        style="--bg-avatar: url('{{ URL::to(config('asset.images_path') . $row->hinh_anh) }}')">
+                                    </div>
 
-                                <div class="d-flex justify-content-end pe-3 avatar-tutor ">
-                                    <img class="avatar-lecture" src="{{ URL::to(config('asset.images_path') . '1.png') }}"
-                                        alt="">
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title hv-link"><a href="" class="text-dark">CĐTH22WEBC - Lập
-                                            trình C#</a></h5>
-                                    <p class="card-text hv-link"><a href="" class="text-dark">Some quick example
-                                            text to build on the card title and
-                                            make
-                                            up
-                                            the bulk of the card's content.</a></p>
-                                </div>
-                                <div class="card-body d-flex justify-content-end">
-                                    <a href="#" class="card-link"><i class="fa fa-file mx-3"></i></a>
-                                    <a href="#" class="card-link"><i class="fa fa-folder"></i></a>
+                                    <div class="d-flex justify-content-end pe-3 avatar-tutor ">
+                                        <img class="avatar-lecture" src="{{ URL::to(config('asset.images_path') . $row->avatar) }}"
+                                            alt="">
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title hv-link"><a href="" class="text-dark">{{$row->ten_lhp}} - {{$row->ten_hp}}</a></h5>
+                                        <p class="card-text hv-link"><a href="" class="text-dark">{{$row->mo_ta}}</a></p>
+                                    </div>
+                                    <div class="card-body d-flex justify-content-end">
+                                        <a href="#" class="card-link"><i class="fa fa-file mx-3"></i></a>
+                                        <a href="#" class="card-link"><i class="fa fa-folder"></i></a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-lg-4 p-3">
-                            <div class="card">
-                                <div class="bg-card card-img-top"
-                                    style="--bg-avatar: url('{{ URL::to(config('asset.images_path') . 'bgrk.png') }}')">
-
-                                </div>
-
-                                <div class="d-flex justify-content-end pe-3 avatar-tutor">
-                                    <img class="avatar-lecture" src="{{ URL::to(config('asset.images_path') . '2.png') }}"
-                                        alt="">
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title hv-link"><a href="" class="text-dark">CĐTH22WEBC - Lập
-                                            trình C#</a></h5>
-                                    <p class="card-text hv-link"><a href="" class="text-dark">Some quick example
-                                            text to build on the card title and
-                                            make
-                                            up
-                                            the bulk of the card's content.</a></p>
-                                </div>
-                                <div class="card-body d-flex justify-content-end">
-                                    <a href="#" class="card-link"><i class="fa fa-file mx-3"></i></a>
-                                    <a href="#" class="card-link"><i class="fa fa-folder"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-lg-4 p-3">
-                            <div class="card">
-                                <div class="bg-card card-img-top"
-                                    style="--bg-avatar: url('{{ URL::to(config('asset.images_path') . 'bgrk.png') }}')">
-
-                                </div>
-
-                                <div class="d-flex justify-content-end pe-3 avatar-tutor">
-                                    <img class="avatar-lecture" src="{{ URL::to(config('asset.images_path') . '3.png') }}"
-                                        alt="">
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title hv-link"><a href="" class="text-dark">CĐTH22WEBC - Lập
-                                            trình C#</a></h5>
-                                    <p class="card-text hv-link"><a href="" class="text-dark">Some quick example
-                                            text to build on the card title and
-                                            make
-                                            up
-                                            the bulk of the card's content.</a></p>
-                                </div>
-                                <div class="card-body d-flex justify-content-end">
-                                    <a href="#" class="card-link"><i class="fa fa-file mx-3"></i></a>
-                                    <a href="#" class="card-link"><i class="fa fa-folder"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-lg-4 p-3">
-                            <div class="card">
-                                <div class="bg-card card-img-top"
-                                    style="--bg-avatar: url('{{ URL::to(config('asset.images_path') . 'bgrk.png') }}')">
-
-                                </div>
-
-                                <div class="d-flex justify-content-end pe-3 avatar-tutor">
-                                    <img class="avatar-lecture" src="{{ URL::to(config('asset.images_path') . '4.png') }}"
-                                        alt="">
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title hv-link"><a href="" class="text-dark">CĐTH22WEBC - Lập
-                                            trình C#</a></h5>
-                                    <p class="card-text hv-link"><a href="" class="text-dark">Some quick example
-                                            text to build on the card title and
-                                            make
-                                            up
-                                            the bulk of the card's content.</a></p>
-                                </div>
-                                <div class="card-body d-flex justify-content-end">
-                                    <a href="#" class="card-link"><i class="fa fa-file mx-3"></i></a>
-                                    <a href="#" class="card-link"><i class="fa fa-folder"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-lg-4 p-3">
-                            <div class="card">
-                                <div class="bg-card card-img-top"
-                                    style="--bg-avatar: url('{{ URL::to(config('asset.images_path') . 'bgrk.png') }}')">
-
-                                </div>
-
-                                <div class="d-flex justify-content-end pe-3 avatar-tutor">
-                                    <img class="avatar-lecture" src="{{ URL::to(config('asset.images_path') . '5.png') }}"
-                                        alt="">
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title hv-link"><a href="" class="text-dark">CĐTH22WEBC - Lập
-                                            trình C#</a></h5>
-                                    <p class="card-text hv-link"><a href="" class="text-dark">Some quick example
-                                            text to build on the card title and
-                                            make
-                                            up
-                                            the bulk of the card's content.</a></p>
-                                </div>
-                                <div class="card-body d-flex justify-content-end">
-                                    <a href="#" class="card-link"><i class="fa fa-file mx-3"></i></a>
-                                    <a href="#" class="card-link"><i class="fa fa-folder"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-lg-4 p-3">
-                            <div class="card">
-                                <div class="bg-card card-img-top"
-                                    style="--bg-avatar: url('{{ URL::to(config('asset.images_path') . 'bgrk.png') }}')">
-
-                                </div>
-
-                                <div class="d-flex justify-content-end pe-3 avatar-tutor">
-                                    <img class="avatar-lecture" src="{{ URL::to(config('asset.images_path') . '6.png') }}"
-                                        width="60px" alt="">
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title hv-link"><a href="" class="text-dark">CĐTH22WEBC - Lập
-                                            trình C#</a></h5>
-                                    <p class="card-text hv-link"><a href="" class="text-dark">Some quick example
-                                            text to build on the card title and
-                                            make
-                                            up
-                                            the bulk of the card's content.</a></p>
-                                </div>
-                                <div class="card-body d-flex justify-content-end">
-                                    <a href="#" class="card-link"><i class="fa fa-file mx-3"></i></a>
-                                    <a href="#" class="card-link"><i class="fa fa-folder"></i></a>
-                                </div>
-                            </div>
-                        </div>
+                        <?php endforeach;
+                        endif;
+                        ?>
                     </div>
                     <!-- card-finish -->
                 </div>
